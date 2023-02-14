614b065d28fe4d546c58d5b79d538635e00e22e4
un-know-n <fa1thlezss@gmail.com>
2023-02-14 12:32:55 +0200

    Task done

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,34 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Class that represents selected task
+ *
+ * @author Yevhenii_D
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    /**
+     * Method that fills the array with prime numbers, using specified range and
+     * outputs it as a string
+     *
+     * @param limit end of the range of prime numbers
+     * @return string
+     */
+    public static String PrimeTheArray(int limit) {
+        if (limit <= 0) {
+            return "Wrong limit!";
+        }
+
+        // Begin the iteration from 2 and up to the limit, filtering by prime 
+        // number, casting everything to an array
+        int primedArray[] = IntStream.iterate(2, i -> i + 1)
+                .filter(j -> IntStream.range(2, j).noneMatch(e -> j % e == 0))
+                .limit(limit)
+                .toArray();
+
+        return Arrays.toString(primedArray);
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,7 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int limit = 5;
+        System.out.println(Exercise.PrimeTheArray(limit));
     }
 }
